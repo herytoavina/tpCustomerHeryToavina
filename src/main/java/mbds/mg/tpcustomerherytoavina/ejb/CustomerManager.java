ca26af2ea89cd8b80b49a18316d914d2ccb8411a
herytoavina <aherytoavina@gmail.com>
2023-03-20 19:54:19 +0300

    Ajout des classes entités + ejb

---
+++ src/main/java/mbds/mg/tpcustomerherytoavina/ejb/CustomerManager.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mbds.mg.tpcustomerherytoavina.ejb;
+
+import jakarta.ejb.Stateless;
+import java.util.List;
+import mbds.mg.tpcustomerherytoavina.entities.Customer;
+
+/**
+ *
+ * @author ahery
+ */
+@Stateless
+public class CustomerManager {
+     public List<Customer> getAllCustomers() {  
+      return null;  
+    }  
+        
+    public Customer update(Customer customer) {
+      return null;  
+    }     
+}
