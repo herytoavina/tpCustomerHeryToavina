0b58346d1f5ba3ea0968a11ba596bc8375b734a9
herytoavina <aherytoavina@gmail.com>
2023-03-28 21:24:30 +0300

    Choix d'un Discount - convertisseur

---
+++ src/main/java/mbds/mg/tpcustomerherytoavina/ejb/DiscountManager.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mbds.mg.tpcustomerherytoavina.ejb;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mbds.mg.tpcustomerherytoavina.entities.Discount;
+
+/**
+ *
+ * @author ahery
+ */
+@Stateless
+public class DiscountManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    
+   public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
+    }
+
+    public void persist(Discount discount) {
+        em.persist(discount);
+    }
+}
